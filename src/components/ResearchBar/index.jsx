71de9b599f0fd0a03c72a5f26f5b1f518226360e
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-28 00:38:42 -0300

    Creating the first Sidebar component and starting to style the ResearchBar

---
+++ src/components/ResearchBar/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function ResearchBar() {
+  return (
+    <input
+      type="search"
+      placeholder="Digite o que você procura"
+      className="slash-search"
+    />
+  );
+}
