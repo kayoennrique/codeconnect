69aff402e57242b3d5bf207a45df9a3c6e14df13
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-28 01:12:12 -0300

    Managing application state and scaling with the sort and filter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import "./App.css";
 import ResearchBar from "./components/ResearchBar";
+import Filter from "./components/Filter";
+import Ordering from "./components/Ordering";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Sidebar />
-      <ResearchBar />
+      <div>
+        <ResearchBar />
+        <Filter />
+        <Ordering />
+      </div>
     </div>
   );
 }
---
+++ src/components/Filter/index.jsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function Filter() {
+  return (
+    <section className="container-filter">
+      <ul>
+        <li>Programação</li>
+      </ul>
+
+      <button>Limpar tudo</button>
+    </section>
+  );
+}
