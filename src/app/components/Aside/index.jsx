07c2aaa742e3eed3c9f2e2c762c29fa3f11bc058
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-27 01:44:43 -0300

    feat(git): creating the Aside component

---
+++ src/app/components/Aside/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./aside.modules.css";
+
+export const Aside = () => {
+  return (
+    <aside className={styles.aside}>
+      <img src="/logo.png" alt="Logo da Code Connect" />
+    </aside>
+  );
+};
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,6 @@
+import { Aside } from './components/Aside';
+import './globals.css';
+
 export const metadata = {
   title: "Code Connect",
   description: "Uma rede social para devs",
@@ -6,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <body>
+        <Aside />
+        {children}
+      </body>
     </html>
   );
 }
