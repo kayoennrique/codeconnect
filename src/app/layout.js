63888de514965676053ef96137f1a369766d5f55
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-27 01:51:41 -0300

    feat(git): using Image component from Next.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,8 +10,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body>
-        <Aside />
-        {children}
+        <div className='app-container'>
+          <Aside />
+          {children}
+        </div>
       </body>
     </html>
   );
