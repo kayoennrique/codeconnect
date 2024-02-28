5bdc1a1b9f28c05e9c91155fb170bb0d809e0197
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-28 15:29:20 -0300

    feat(git): creating fetch function

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,19 @@
 import { CardPost } from "./components/CardPost";
 
-const post = {
-  "id": 1,
-  "cover": "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/posts/introducao-ao-react.png",
-  "title": "Introdução ao React",
-  "slug": "introducao-ao-react",
-  "body": "Neste post, vamos explorar os conceitos básicos do React, uma biblioteca JavaScript para construir interfaces de usuário. Vamos cobrir componentes, JSX e estados.",
-  "markdown": "```javascript\nfunction HelloComponent() {\n  return <h1>Hello, world!</h1>;\n}\n```",
-  "author": {
-    "id": 101,
-    "name": "Ana Beatriz",
-    "username": "anabeatriz_dev",
-    "avatar": "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/authors/anabeatriz_dev.png"
+async function getAllPosts() {
+  const response = await fetch('http://localhost:3042/posts');
+  if (!response.ok) {
+    console.log('Ops, alguma coisa correu mal');
   }
+  return response.json();
 }
 
+export default async function Home() {
+  const posts = await getAllPosts();
 
-export default function Home() {
   return (
     <main>
-      <CardPost post={post} />
+      {posts.map(post => <CardPost post={post} />)}
     </main>
   );
 }
