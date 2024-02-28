2e33470da43afacd67825776601f1235113e8ee8
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-28 14:57:45 -0300

    feat(git): configuring external images

--- src/app/components/Avatar/index.jsx
+++ src/app/components/Avatar/index.jsx
@@ -4,7 +4,12 @@ export const Avatar = ({ name, imageSrc }) => {
   return (
     <ul>
       <li>
-        <Image src={imageSrc} width={32} height={32} />
+        <Image
+          src={imageSrc}
+          width={32}
+          height={32}
+          alt={`Avatar do(a) ${name}`}
+        />
       </li>
       <li>@{name}</li>
     </ul>
--- src/app/components/CardPost/index.jsx
+++ src/app/components/CardPost/index.jsx
@@ -6,7 +6,12 @@ export const CardPost = ({ post }) => {
     <article>
       <header>
         <figure>
-          <Image src={post.cover} width={438} height={133} />
+          <Image
+            src={post.cover}
+            width={438}
+            height={133}
+            alt={`Capa do post de titulo: ${post.title}`}
+          />
         </figure>
       </header>
       <section>
