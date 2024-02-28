d6007074c7fe24f8f2a568b2736642d640d0d754
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-28 15:11:06 -0300

    feat(git): styling the card

--- src/app/components/Avatar/index.jsx
+++ src/app/components/Avatar/index.jsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import styles from "./avatar.modules.css";
 
 export const Avatar = ({ name, imageSrc }) => {
   return (
-    <ul>
+    <ul className={styles.avatar}>
       <li>
         <Image
           src={imageSrc}
--- src/app/components/CardPost/index.jsx
+++ src/app/components/CardPost/index.jsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import { Avatar } from "../Avatar";
+import styles from "./cardpost.module.css";
 
 export const CardPost = ({ post }) => {
   return (
-    <article>
-      <header>
+    <article className={styles.card}>
+      <header className={styles.header}>
         <figure>
           <Image
             src={post.cover}
@@ -14,11 +15,11 @@ export const CardPost = ({ post }) => {
           />
         </figure>
       </header>
-      <section>
+      <section className={styles.body}>
         <h2>{post.title}</h2>
         <p>{post.body}</p>
       </section>
-      <footer>
+      <footer className={styles.footer}>
         <Avatar imageSrc={post.author.avatar} name={post.author.username} />
       </footer>
     </article>
