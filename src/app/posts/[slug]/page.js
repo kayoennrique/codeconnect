29ebd89fc12bc1ee2365b1e470fd52e75f0ca346
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-28 19:31:25 -0300

    feat(git): getting the post asynchronously

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -1,5 +1,25 @@
-const PagePost = () => {
-  return <h1>Olá posts</h1>
+import logger from "@/logger";
+
+async function getPostBySlug(slug) {
+  const url = `http://localhost:3042/posts?slug=${slug}`;
+  const response = await fetch(url)
+  if (!response.ok) {
+    logger.error('Ops, alguma coisa correu mal')
+    return {}
+  }
+  logger.info('Posts obtidos com sucesso')
+  const data = await response.json()
+  if (data.length == 0) {
+    return {}
+  }
+
+  return data[0];
+}
+
+
+const PagePost = async ({ params }) => {
+  const post = await getPostBySlug(params.slug);
+  return <h1 style={{ color: 'white' }}>{post.title}</h1>
 }
 
 export default PagePost;
