f4663168e9533351df19d10bdfb156d4ef7d968a
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-28 15:02:58 -0300

    feat(git): importing Google Fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { Prompt } from 'next/font/google';
 import { Aside } from './components/Aside';
 import './globals.css';
 
@@ -6,9 +7,15 @@ export const metadata = {
   description: "Uma rede social para devs",
 };
 
+const prompt = Prompt({
+  weight: ['400', '600'],
+  subsets: ['latin'],
+  display: 'swap'
+});
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className={prompt.className}>
       <body>
         <div className='app-container'>
           <Aside />
