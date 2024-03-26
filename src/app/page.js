028fb62e3f0c9267d04df453d9cbe6e022c55f57
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-03-26 15:43:07 -0300

    feat(git): getting Posts from Postgres

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,20 @@
-import { CardPost } from "@/components/CardPost"
-import logger from "@/logger"
+import { CardPost } from "@/components/CardPost";
+import logger from "@/logger";
 
-import styles from './page.module.css'
-import Link from "next/link"
+import styles from './page.module.css';
+import Link from "next/link";
+import db from "../../prisma/db";
 
 async function getAllPosts(page) {
-  const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`)
-  if (!response.ok) {
-    logger.error('Ops, alguma coisa correu mal')
-    return []
+  try {
+    const posts = await db.post.findMany();
+
+    return { data: posts, prev: null, next: null };
+
+  } catch (error) {
+    logger.error('Falha ao obter posts', { error })
+    return { data: [], prev: null, next: null };
   }
-  logger.info('Posts obtidos com sucesso')
-  return response.json()
 }
 
 export default async function Home({ searchParams }) {
@@ -25,5 +28,5 @@ export default async function Home({ searchParams }) {
         {next && <Link href={`/?page=${next}`}>Próxima página</Link>}
       </div>
     </main>
-  )
+  );
 }
