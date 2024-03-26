5121a7cdb53f07c60945be2e6d8a2ef89e1abfa5
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-03-26 18:39:44 -0300

    feat(git): paging the query result

--- src/app/page.js
+++ src/app/page.js
@@ -6,17 +6,26 @@ import Link from "next/link";
 import db from "../../prisma/db";
 
 async function getAllPosts(page) {
+
   try {
-    const perPage = 6;
+
+    const perPage = 4;
+    const skip = (page - 1) * perPage;
+    const totalItems = await db.post.count();
+    const totalPages = Math.ceil(totalItems / perPage);
+    const prev = page > 1 ? page - 1 : null;
+    const next = page < totalPages ? page + 1 : null;
+
     const posts = await db.post.findMany({
       take: perPage,
+      skip,
       orderBy: { createdAt: 'desc' },
       include: {
         author: true
       }
     })
 
-    return { data: posts, prev: null, next: null }
+    return { data: posts, prev, next }
 
   } catch (error) {
     logger.error('Falha ao obter posts', { error })
@@ -25,8 +34,10 @@ async function getAllPosts(page) {
 }
 
 export default async function Home({ searchParams }) {
-  const currentPage = searchParams?.page || 1
-  const { data: posts, prev, next } = await getAllPosts(currentPage)
+
+  const currentPage = parseInt(searchParams?.page || 1);
+  const { data: posts, prev, next } = await getAllPosts(currentPage);
+
   return (
     <main className={styles.grid}>
       {posts.map(post => <CardPost key={post.id} post={post} />)}
