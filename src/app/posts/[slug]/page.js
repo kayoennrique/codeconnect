940ca69b4051c8b081cff00fb4b418ff2d2f0336
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-03-27 00:50:52 -0300

    feat(git): query with findFirst

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -1,25 +1,21 @@
-import logger from "@/logger"
 import { remark } from 'remark';
 import html from 'remark-html';
 
 import styles from './page.module.css'
 import { CardPost } from "@/components/CardPost";
+import db from "../../../../prisma/db";
 
 
 async function getPostBySlug(slug) {
-  const url = `http://localhost:3042/posts?slug=${slug}`
-  const response = await fetch(url)
-  if (!response.ok) {
-    logger.error('Ops, alguma coisa correu mal')
-    return {}
-  }
-  logger.info('Posts obtidos com sucesso')
-  const data = await response.json()
-  if (data.length == 0) {
-    return {}
-  }
 
-  const post = data[0];
+  const post = await db.post.findFirst({
+    where: {
+      slug
+    },
+    include: {
+      author: true
+    }
+  })
 
   const processedContent = await remark()
     .use(html)
@@ -28,18 +24,22 @@ async function getPostBySlug(slug) {
 
   post.markdown = contentHtml
 
-  return post
+  return post;
 }
 
 const PagePost = async ({ params }) => {
-  const post = await getPostBySlug(params.slug)
-  return (<div>
-    <CardPost post={post} highlight />
-    <h3 className={styles.subtitle}>Código:</h3>
-    <div className={styles.code}>
-      <div dangerouslySetInnerHTML={{ __html: post.markdown }} />
+
+  const post = await getPostBySlug(params.slug);
+
+  return (
+    <div>
+      <CardPost post={post} highlight />
+      <h3 className={styles.subtitle}>Código:</h3>
+      <div className={styles.code}>
+        <div dangerouslySetInnerHTML={{ __html: post.markdown }} />
+      </div>
     </div>
-  </div>)
+  );
 }
 
-export default PagePost
+export default PagePost;
