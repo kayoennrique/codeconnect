df559c2074e8b6a6700aaabf77f53cadaaec7331
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-03-27 01:19:49 -0300

    feat(git): parameters in links

--- src/app/page.js
+++ src/app/page.js
@@ -53,8 +53,8 @@ export default async function Home({ searchParams }) {
     <main className={styles.grid}>
       {posts.map(post => <CardPost key={post.id} post={post} />)}
       <div className={styles.links}>
-        {prev && <Link href={`/?page=${prev}`}>Página anterior</Link>}
-        {next && <Link href={`/?page=${next}`}>Próxima página</Link>}
+        {prev && <Link href={{ pathname: '/', query: { page: prev, q: searchTerm } }}>Página anterior</Link>}
+        {next && <Link href={{ pathname: '/', query: { page: next, q: searchTerm } }}>Próxima página</Link>}
       </div>
     </main>
   );
