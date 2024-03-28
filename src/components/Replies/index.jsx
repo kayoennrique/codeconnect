781a67f611f1c4fe46e448fc50386da2052308ca
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-03-28 01:52:05 -0300

    feat(git): performance care

--- src/components/Replies/index.jsx
+++ src/components/Replies/index.jsx
@@ -1,12 +1,26 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./replies.module.css";
 import { Comment } from "../Comment";
 import { ReplyModal } from "../ModalReply";
 
 export const Replies = ({ comment }) => {
   const [showReplies, setShowReplies] = useState(false);
+
+  const [replies, setReplies] = useState([]);
+
+  async function fetchData() {
+    const response = await fetch(`/api/comment/${comment.id}/replies`);
+    const data = await response.json();
+    setReplies(data);
+  }
+
+  useEffect(() => {
+    if (showReplies) {
+      fetchData();
+    }
+  }, [showReplies]);
 
   return (
     <div className={styles.container}>
@@ -19,7 +33,7 @@ export const Replies = ({ comment }) => {
         </button>
         {showReplies && (
           <ul>
-            {comment.children.map((reply) => (
+            {replies.map((reply) => (
               <li key={reply.id}>
                 <Comment comment={reply} />
                 <ReplyModal comment={reply} />
